ac1e86cc2fbc5c259dc05f43bd753758ef7dcbbf
Daniel Micay <daniel.micay@grapheneos.org>
2024-02-02 18:54:40 -0500

    run full explicit GC in SystemUI and system_server after locking
    ART has special support for System.gc() and Android already uses this to
    purge memory after unlocking. It makes sense to purge as much as we can
    when locking too.
    
    Change-Id: I259d1d3391621459754dd3c49469cd0f140d8bfc

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2777,6 +2777,9 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             userActivity();
             mUpdateMonitor.setKeyguardGoingAway(false);
             mKeyguardViewControllerLazy.get().setKeyguardGoingAwayState(false);
+            System.gc();
+            System.runFinalization();
+            System.gc();
             mShowKeyguardWakeLock.release();
         }
         mKeyguardDisplayManager.show();
